03d2495a18c689f9c8803765e294ce5a40102db0
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-11 21:31:24 -0500

    Update pell.py

--- pell.py
+++ pell.py
@@ -1,3 +1,11 @@
 def pell(n):
     index = 2
     pell_val =[0,1]
+    adding = 1
+    for index in range(n-1):
+        if n<0:
+            return
+        adding =(2 * adding + pell_val[-2])
+        pell_val.append(adding)                 
+        # print(index)
+
